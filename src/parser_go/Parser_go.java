c8b9b2a4eee287b6afc1db1b1419550159777ac3
Sergei <art-sa@bk.ru>
2015-12-12 17:12:53 +0100

    Что-то изменено

--- src/parser_go/Parser_go.java
+++ src/parser_go/Parser_go.java
@@ -5,6 +5,11 @@
  */
 package parser_go;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Sergey
@@ -14,8 +19,19 @@ public class Parser_go {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        try {
+            javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(main_window.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Parser_go.class.getName()).log(Level.SEVERE, null, ex);
+        }
         // TODO code application logic here
+        main_window main_window_class = new main_window();
+        main_window_class.setLocation(10, 10);
+        main_window_class.setVisible(true);
+        
     }
     
 }
